3f4817a8c6b6490425234d0ca3efc17794f32120
shirishphatangare <sphatangare@gmail.com>
2018-06-16 19:47:03 -0500

    removed folder No_test
    removed folder No_test

--- employee-details-service_no_Test/src/main/java/com/example/employeedetailsservice/service/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeedetailsservice.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.employeedetailsservice.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-	
-}
