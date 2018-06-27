ba9904cf96fb4e2c1590b3fcbe663433d278fbc3
shirishphatangare <sphatangare@gmail.com>
2018-06-26 19:11:09 -0500

    added comment

--- employee-details-service/src/main/java/com/example/employeedetailsservice/EmployeeDetailsServiceApplication.java
+++ employee-details-service/src/main/java/com/example/employeedetailsservice/EmployeeDetailsServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class EmployeeDetailsServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Inside EmployeeDetailsServiceApplication..");
 		SpringApplication.run(EmployeeDetailsServiceApplication.class, args);
 	}
 
